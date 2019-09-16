db9a5af6c06991af721f4364de0de5bd49d31b4f
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-16 09:15:35 +0900

    Router user/:name path

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	router.Run()
 }
